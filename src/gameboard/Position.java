9364f77402f51c1bfb3bdc32768750be59d9d493
amac81 <arnaldocanelas@gmail.com>
2023-06-24 19:23:22 +0100

    class Position changes

--- src/gameboard/Position.java
+++ src/gameboard/Position.java
@@ -1,9 +1,18 @@
 package gameboard;
 
+/*OOP Topics
+ * 
+ * Encapsulation
+ * Constructors
+ * ToString method (Object / override)
+ * 
+ */
+
+//this class represents a position in the game board
 public class Position {
 	private int row;
 	private int column;
-	
+
 	public Position(int row, int column) {
 		this.row = row;
 		this.column = column;
@@ -12,9 +21,17 @@ public class Position {
 	public int getRow() {
 		return row;
 	}
-	
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
 	public int getColumn() {
 		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
 	}
 
 	public void setValues(int row, int column) {
@@ -22,5 +39,9 @@ public class Position {
 		this.column = column;
 	}
 	
+	public String toString() {
+		return row + ", " + column;
+	}
 	
+
 }
